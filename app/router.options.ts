52e35b02953814bdac057d7f13e19d7b93e0afea
Serhii <bedrytskyi@gmail.com>
2022-08-01 16:56:33 +0300

    try promise

--- app/router.options.ts
+++ app/router.options.ts
@@ -2,13 +2,32 @@ import type { RouterConfig } from '@nuxt/schema'
 
 // // https://router.vuejs.org/api/#routeroptions
 export default <RouterConfig>{
+  // blinks with "from" page first screen
   scrollBehavior: (to, _, savedPosition) => {
-   
-    // Scroll to top of window
     if (savedPosition) {
       return savedPosition
     } else {
       return { top: 0 }
     }
   }
+
+  // // blinks with "to" page second screen (because savedPosition moved to next page)
+  // scrollBehavior(to, _from, savedPosition) {
+  //   return new Promise((resolve, _reject) => {
+  //     setTimeout(() => {
+  //       if (savedPosition) {
+  //         resolve(savedPosition)
+  //       } else {
+  //         if (to.hash) {
+  //           resolve({
+  //             el: to.hash,
+  //             top: 80
+  //           })
+  //         } else {
+  //           resolve({ top: 0 })
+  //         }
+  //       }
+  //     }, 100)
+  //   })
+  // }
 }
